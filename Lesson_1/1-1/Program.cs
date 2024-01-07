026715687930080414e5ef56b01b7c6d7d1d8133
Dudovitskiy <udovickiy1989@gmail.com>
2024-01-07 17:34:49 +0300

    Lesson 1 task 2

--- Lesson_1/1-1/Program.cs
+++ Lesson_1/1-1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// 1. Напиште программу, которая на вход принимает два числа и проверяет,
 //    является ли первое число квадратом второго.
 
-Console.Write("Enter a number 1");
+Console.Write("Enter a number 1: ");
 int b1 = int.Parse(Console.ReadLine()!);
-Console.Write("Enter a number 2");
+Console.Write("Enter a number 2: ");
 int b2 = int.Parse(Console.ReadLine()!);
 if (b1 * b1 == b2)
     Console.WriteLine("Yes");
---
+++ Lesson_1/1-2/Program.cs
@@ -0,0 +1,11 @@
+﻿// 2. Напишите программу, которая на вход принемает целое число N,
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Enter a number 1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+int count = -b1;
+while (count <= b1)
+{
+    Console.Write(count+ " ");
+    count++;
+}
