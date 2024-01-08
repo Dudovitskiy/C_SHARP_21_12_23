fdd9e29559805ca895f61825317ec63c3be3b9e3
Dudovitskiy <udovickiy1989@gmail.com>
2024-01-08 19:13:27 +0300

    Lesson 1 task 3

---
+++ Lesson_1/1-3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает сумму первой и последней цифры.
+
+Console.WriteLine("Enter a number: ");
+int s = int.Parse(Console.ReadLine()!);
+int Last = s % 10;
+int First = s / 100;
+int sum = First + Last;
+Console.WriteLine(sum);
