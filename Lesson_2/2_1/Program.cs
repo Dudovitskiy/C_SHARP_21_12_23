a7c99866d7d1e18ba783deff817b382f0151cc83
Dudovitskiy <udovickiy1989@gmail.com>
2024-01-08 22:02:48 +0300

    Lesson 2

--- Lesson_2/2_1/Program.cs
+++ Lesson_2/2_1/Program.cs
@@ -22,34 +22,6 @@
 
 //Console.WriteLine(a / 1000);
 
-//int a = int.Parse(Console.ReadLine()!);
-//Console.WriteLine(a / 100 * 10 + a % 10);
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a / 100 * 10 + a % 10);
 
-// 2. Напищите прогрмамму, которая принимает вход трёхзначное число
-//    и возводит вторую цифру этого числа в степень, равную третьей цифре.
-
-//Console.WriteLine("Введите трехзначное число");
-//int num = Convert.ToInt32(Console.ReadLine());
-
-//if (num >= 100 && num < 1000)
-//{
-//    int num1 = num % 100 / 10;
-//    int num2 = num % 10;
-//    double result = Math.Pow(num1, num2);
-//    Console.WriteLine(result);
-//}
-//else
-//{
-//    if (num < 100 || num > 1000)
-//    {
-//        Console.WriteLine("Вы ввели не трехзначное число");
-//    }
-//}
-
-//Console.WriteLine("Введите трехзначное число:");
-//int s = int.Parse(Console.ReadLine()!);
-//Console.WriteLine(Math.Pow(s % 100 / 10, s % 10));
-
-Console.WriteLine("Введите трехзначное число:");
-int s = int.Parse(Console.ReadLine()!);
-Console.WriteLine(Math.Pow(s / 10 % 10, s % 10));
