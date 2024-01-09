918f236c26d09ea4b996d03e4fbdd2b5a4fa5402
Dudovitskiy <udovickiy1989@gmail.com>
2024-01-09 13:43:08 +0300

    HW_4

---
+++ Lesson_2/HW_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// 4. Напишите программу, которая на вход принемает натуральное число N,
+//    а на выходе показывает его цифры через запятую.  + "\n"
+
+
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine()!);
+string a = "";
+if (n > 0)
+{
+    while (n > 0)
+    {
+        a = n % 10 + "," + a;
+        n = n / 10;     
+    }
+ Console.Write(a.Trim(',') + "\n");
+}
+else
+    Console.Write("Некоректное число");
