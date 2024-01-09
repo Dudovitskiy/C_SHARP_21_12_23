d46801b126df9c6225956117f6bd876f820e4c09
Dudovitskiy <udovickiy1989@gmail.com>
2024-01-09 11:02:56 +0300

    HW_2

--- Lesson_2/HW_2/Program.cs
+++ Lesson_2/HW_2/Program.cs
@@ -1,22 +1,28 @@
-﻿Console.WriteLine("Enter a numbers:");
+﻿// 3. Напишите программу, которая принемает на вход кординаты точки (x,y)
 
+Console.WriteLine("Введите кординату x");
 int x = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите кординату y");
 int y = int.Parse(Console.ReadLine()!);
 
-if (x > 0 && y > 0)
+if (x != 0 || y != 0)
 {
-    Console.WriteLine("1");
+    Console.WriteLine("Ошибка кординат");
+}    
+else if (x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находится на оси кординат 1");
 }
 else if (x < 0 && y > 0)
 {
-    Console.WriteLine("2");
+    Console.WriteLine("Точка находится на оси кординат 2");
 }
 else if (x < 0 && y < 0)
 {
-    Console.WriteLine("3");
+    Console.WriteLine("Точка находится на оси кординат 3");
 }
 else if (x > 0 && y < 0)
 {
-    Console.WriteLine("4");
+    Console.WriteLine("Точка находится на оси кординат 4");
 }
-Console.WriteLine("Точка находится на оси кординат");
